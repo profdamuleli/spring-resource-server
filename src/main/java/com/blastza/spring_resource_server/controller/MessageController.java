d2958cbd3d2672f4c81dcc9ed9c49e6cdd3374ec
Lutendo Damuleli <lutendo.damuleli@outlook.com>
2024-07-04 03:59:25 +0200

    refactors

--- src/main/java/com/blastza/spring_resource_server/controller/MessageController.java
+++ src/main/java/com/blastza/spring_resource_server/controller/MessageController.java
@@ -1,13 +1,22 @@
 package com.blastza.spring_resource_server.controller;
 
+import com.blastza.spring_resource_server.service.GreetingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@RequiredArgsConstructor
 public class MessageController {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello from resource server";
+    private final GreetingService greetingService;
+
+    @GetMapping("/")
+    @ResponseBody
+    public Map<String, String> hello(){
+        return greetingService.greet();
     }
 }
